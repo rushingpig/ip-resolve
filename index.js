bc3880296b0f2a1a23a9a23fd410370119e8ca81
Eric.zhu <rushingpig@163.com>
2015-06-11 23:59:52 +0800

    初始化导入

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @author Eric.zhu
+ * @email  Eric.zhu@sz-seastar.com
+ * @date   2015/6/8 0008 14:19
+ * @des    默认的入口
+ */
+var IPResolve = require('./lib/common/IPResolve');
+
+module.exports = IPResolve;
